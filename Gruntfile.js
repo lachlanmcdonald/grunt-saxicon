1408701406f46cc11ad0c83fb93d74094ed733a1
Lachlan McDonald <lacmcdonald@deloitte.com.au>
2016-01-19 14:57:16 +1100

    Config for easier testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,57 +8,73 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-    // Configuration to be run (and then tested).
-    saxicon: {
-      default: {
-      	options: {
-      		source: "test/src",
-      		json: "tmp/data.json",
-      		scss: {
-      			output: "tmp/_saxicon.scss"
-      		}
-      	}
-      }
-    },
+		// Configuration to be run (and then tested).
+		saxicon: {
+			json: {
+				options: {
+					source: "test/src",
+					json: "tmp/data.json",
+				}
+			},
+			scss: {
+				options: {
+					source: "test/src",
+					scss: {
+						output: "tmp/_saxicon.scss"
+					}
+				}
+			},
+			svgs: {
+				options: {
+					source: "test/src",
+					svgs: {
+						output: "tmp/svgs/",
+						colors: {
+							red: "#F00",
+							blue: "#00F",
+							green: "#0F0"
+						}
+					}
+				}
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
+	});
 
-  });
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'saxicon', 'nodeunit']);
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'saxicon', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['clean', 'jshint', 'test']);
-
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['clean', 'jshint', 'test']);
 };
