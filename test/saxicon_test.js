ac3bcadc5caf8366162af516e81c3b60f9cbf70f
Lachlan McDonald <lacmcdonald@deloitte.com.au>
2016-01-26 23:22:25 +1100

    Added tests for SVG output and simplified JSON tests

--- test/saxicon_test.js
+++ test/saxicon_test.js
@@ -3,48 +3,73 @@
 var grunt = require('grunt'),
 	exec = require('child_process').exec,
 	path = require('path'),
-	srcCount = 8,
+	_ = require('lodash'),
 	execOptions;
 
 execOptions = {
 	cwd: path.join(__dirname, '..')
 };
 
+
 exports.saxicon = {
 	setUp: function(done) {
+		this.svgs = grunt.file.expand({
+			cwd: 'test/src'
+		}, '*.svg').map(function(x) {
+			return [path.basename(x, '.svg'), '.svg'];
+		});
+
 		done();
 	},
 	tearDown: function(done) {
-		done();
-	},
-	test_json_valid: function(test) {
-		test.expect(1);
-
-		exec('grunt saxicon:test_json', execOptions, function(error, stdout) {
-			test.doesNotThrow(function() {
-				grunt.file.readJSON('tmp/data.json');
-			});
-
-			test.done();
+		exec('grunt clean', execOptions, function() {
+			done();
 		});
 	},
-	test_json_structure: function(test) {
-		test.expect(1 + 6 * srcCount);
+	test_svgs: function(test) {
+		var colors = _.keys(grunt.config('saxicon.test_svgs.options.svgs.colors')),
+			output;
+
+		test.expect(1 + (this.svgs.length * colors.length));
+
+		exec('grunt saxicon:test_svgs', execOptions, function(error, stdout) {
+			var svgs = grunt.file.expand({
+				cwd: 'tmp/svgs'
+			}, '*.svg');
+
+			test.ok(svgs.length === this.svgs.length * colors.length);
+
+			colors.forEach(function(color) {
+				this.svgs.forEach(function(a) {
+					var fname = a[0] + '.' + color + a[1];
+					test.notEqual(svgs.indexOf(fname), -1);
+				});
+			}.bind(this));
+
+			test.done();
+		}.bind(this));
+	},
+	test_json: function(test) {
+		test.expect(2 + 6 * this.svgs.length);
 
 		exec('grunt saxicon:test_json', execOptions, function(error, stdout) {
-			var data = grunt.file.readJSON('tmp/data.json');
+			var data;
+
+			test.doesNotThrow(function() {
+			 	data = grunt.file.readJSON('tmp/data.json');
+			});
 			test.equal(data.length, 8);
 
 			data.forEach(function(x) {
-				test.equal(typeof x.width, "number");
-				test.equal(typeof x.height, "number");
-				test.equal(typeof x.components, "object");
-				test.equal(typeof x.path, "string");
-				test.equal(typeof x.icon, "string");
+				test.equal(typeof x.width, 'number');
+				test.equal(typeof x.height, 'number');
+				test.equal(typeof x.components, 'object');
+				test.equal(typeof x.path, 'string');
+				test.equal(typeof x.icon, 'string');
 				test.ok(x.components.length > 0);
-			});
+			}.bind(this));
 
 			test.done();
-		});
+		}.bind(this));
 	}
 };
