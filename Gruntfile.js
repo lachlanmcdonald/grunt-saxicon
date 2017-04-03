c6438bcea16332209249da50595f41377de1ed79
Lachlan McDonald <lachlan@radelaide.net>
2017-04-03 20:58:29 +1000

    RF Removed jshint
    Prefer eslint config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,19 +9,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-
-	// Project configuration.
 	grunt.initConfig({
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'tasks/*.js',
-				'<%= nodeunit.tests %>'
-			],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		},
 
 		// Before generating any new files, remove any previously-created files.
 		clean: {
@@ -162,7 +150,6 @@ module.exports = function(grunt) {
 	grunt.loadTasks('tasks');
 
 	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
@@ -171,5 +158,5 @@ module.exports = function(grunt) {
 	grunt.registerTask('test', ['clean', 'nodeunit']);
 
 	// By default, lint and run all tests.
-	grunt.registerTask('default', ['clean', 'jshint', 'test']);
+	grunt.registerTask('default', ['clean', 'test']);
 };
